254fb16404f89ec57c5a0e310873723b0c707618
sangeon22 <tkddjsdl33@hanmail.net>
2023-05-02 20:56:53 +0900

    Study: Spring Data JPA를 사용한 DB 접근

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,12 +1,10 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.persistence.EntityManager;
 
 @Configuration
 public class SpringConfig {
@@ -19,25 +17,33 @@ public class SpringConfig {
     }*/
 
 //    @PersistenceContext
-    private EntityManager em;
+   /* private EntityManager em;
 
     public SpringConfig(EntityManager em) {
         this.em = em;
+    }*/
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+//        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-        // 인터페이스 X 구현체 생성
-        // return new MemoryMemberRepository();
-
-        // 이 구현체만 교체해주면 된다!!!
-        // return new JdbcMemberRepository(dataSource);
-        // return new JdbcMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        // 인터페이스 X 구현체 생성
+//        // return new MemoryMemberRepository();
+//
+//        // 이 구현체만 교체해주면 된다!!!
+//        // return new JdbcMemberRepository(dataSource);
+//        // return new JdbcMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+//    }
 }
